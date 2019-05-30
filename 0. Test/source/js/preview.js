3003b15541c2f28544ceafa8b982d5c2f897ba1f
Антон <tat7106@gmail.com>
2019-05-30 11:52:54 +0600

    done page

---
+++ 0. Test/source/js/preview.js
@@ -0,0 +1,9 @@
+'use strict';
+(function () {
+  // константы
+  // высота окна
+  var HEIGHT_WINDOW = document.documentElement.clientHeight;
+
+  var header = document.querySelector('.main-header');
+  header.style.height = HEIGHT_WINDOW + 'px';
+})();
