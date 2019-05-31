4476f5cd87f028de4e098e899895dd28adff44ac
Антон <tat7106@gmail.com>
2019-05-31 16:29:53 +0600

    fixed background and br

--- 0. Test/source/js/preview.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-(function () {
-  // константы
-  // высота окна
-  var HEIGHT_WINDOW = document.documentElement.clientHeight;
-
-  var header = document.querySelector('.main-header');
-  header.style.height = HEIGHT_WINDOW + 'px';
-})();
